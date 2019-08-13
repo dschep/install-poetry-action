7446008b2950b388606a1fd9b85edf0418611fe7
Daniel Schep <dschep@gmail.com>
2019-08-13 10:30:54 -0400

    safe file to disk

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function run() {
     const version = core.getInput('version');
     const preview = core.getInput('preview');
 
-    await exec('curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py');
+    await exec('curl -O -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py');
 
     const flags = preview ? '--preview' : version ? `--version=${version}`: '';
     await exec(`python get-poetry.py --yes ${flags}`)
